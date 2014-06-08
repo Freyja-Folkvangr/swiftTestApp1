ba17e3631b5209526fda30d9488cae86b6d19370
Giuliano P. Tognarelli B <giuliano150@outlook.com>
2014-06-08 13:39:54 -0400

    Initial Commit

---
+++ swiftApp1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftApp1
+//
+//  Created by Giuliano on 6/8/14.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
